0c37f562c8b51d7966527b63529ee2ad7c677be3
j.yoshikawa <j.yoshikawa@takionsd.com>
2023-04-18 12:49:23 +0900

    add page

--- myapp.rb
+++ myapp.rb
@@ -2,5 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
  
 get '/' do
-    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a>"
+    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/add'>add</a>"
+end
+
+get '/add' do
+    "<h1>Hello World!</h1><p>igoistoirgdiutghsl</p><a href='/'>index</a>"
 end
